f2e4c0c2a457029def7062ce42fa5718d10f3823
Mendrika <77017214+mendrika-ops@users.noreply.github.com>
2023-07-24 23:36:52 +0300

    splash

--- Tongasoa/app/src/main/java/com/example/tongasoa/MainActivity.java
+++ Tongasoa/app/src/main/java/com/example/tongasoa/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.tongasoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tongasoa.vue.Login;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000;
+    Animation boutom, top;
+    ImageView image;
+    TextView logo,slogan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        boutom = AnimationUtils.loadAnimation(this, R.anim.animation_boutom);
+        top = AnimationUtils.loadAnimation(this, R.anim.animation_top);
+
+        image = findViewById(R.id.imageView);
+
+        image.setAnimation(top);
+
+        new Handler().postDelayed(()-> {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+        },SPLASH_SCREEN);
+
     }
 }
